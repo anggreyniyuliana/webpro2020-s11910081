5af5c0c65183fdb25e5dd554e60e0704385940ff
anggreyniyuliana <anggreyni180@gmail.com>
2020-11-12 09:30:57 +0800

    20

---
+++ pertemuan20/indeks.js
@@ -0,0 +1,64 @@
+//console.log("hello javascript");
+
+//Variabel
+
+//let & const
+/*let umur = 25;
+let nilai = 95.5; //number
+let sudahMenikah = true; //boolean
+let nama = "John";
+console.log(umur);
+console.log(nama);
+
+let alamat;
+console.log(alamat);
+
+const tahunLahir = 2010;
+console.log(tahunLahir); 
+
+//Operator
+
+//Aritmatika Operator (+,-,/,x)
+
+let angka1 = 18;
+let angka2 = 20;
+console.log(angkat1 + angka2);
+
+let namaDepan ="Anggreyni";
+let namaBelakang ="Yuliana";
+
+console.log("Nama Saya", namaDepan + " " + namaBelakang + ".", "Umur Saya adalah", umur + "tahun");
+
+//Operator perbandingan
+
+let angka1 = 12; //number
+let angka2 = "10"; //string
+
+let hasil = angka1 == angka2; // == cek tanpa memperhatikan tipe data
+console.log(angka1 == angka2);
+
+//ternary operator
+let angka = 5;
+let hasil = angka < 0 ? "Negatif" : "Positif";
+console.log(hasil);
+
+//Operator logika
+
+let kondisi1 = true;
+let kondisi2 = false;
+
+console.log(kondisi1 || kondisi2);
+console.log(!kondisi1); */
+
+let angka = prompt("Masukkan angka :");
+console.log("Angka yang dimasukkan adalah =" + angka);
+
+//Exercise 
+
+/* Buatlah program Javasrcipt untuk menghitung BMI (Body Mess Index).
+ Rumus = Berat (kg) / (Tinggi * Tinggi)  */
+
+ let berat = prompt("Masukkan Berat badan (kg)");
+ let tinggi = prompt("Masukkan Tinggi badan (cm) :");
+let bmi = berat/(tinggi*tinggi);
+console.log("BMI =" + bmi); 
